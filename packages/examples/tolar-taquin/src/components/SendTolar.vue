0f158068717162469b42a3b47722f684e3ccc83a
Ana Blazevic <ana.blazevic@mooncode.hr>
2026-03-19 14:31:07 +0100

    Adding user friendly Tolar methods

--- packages/examples/tolar-taquin/src/components/SendTolar.vue
+++ packages/examples/tolar-taquin/src/components/SendTolar.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <p>Balance: {{ balance }} TOL</p>
+    <button v-if="!walletConnected" @click="connectWallet">
+      Connect Wallet
+    </button>
+
+    <p v-if="walletConnected">Balance: {{ balance }} TOL</p>
+    <p v-if="walletConnected">Account: {{ account }}</p>
     
     <input v-model="receiver" placeholder="Receiver Address" />
     <input v-model="amount" type="number" placeholder="Amount" />
@@ -13,87 +18,121 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, onMounted } from 'vue';
+import { defineComponent, ref } from 'vue';
 import { NetworkId } from "@tolar/web3-plugin-tolar";
-
 
 export default defineComponent({
   setup() {
+
     const receiver = ref('');
     const amount = ref('');
     const balance = ref('0');
     const error = ref('');
     const success = ref('');
+    const network = ref('');
+
+    const walletConnected = ref(false); // ADDED
 
     // @ts-ignore
     let taquin;
-    let account = '';
+    const account = ref ('');
 
-    const initTaquin = async () => {
-      // @ts-ignore
-      taquin = window.taquin;
-      // @ts-ignore
-      const accounts = await taquin.tolar.request({method: 'tol_requestAccounts'});
-      account = accounts[0];
-    }
+    // NEW FUNCTION
+    const connectWallet = async () => {
+      try {
+        // @ts-ignore
+        taquin = window.taquin;
+
+        // @ts-ignore
+     const accounts = await taquin.tolar.requestAccounts();
+
+        account.value = accounts[0];
+        walletConnected.value = true;
+
+        await loadNetwork();
+        await loadBalance();
+
+      } catch (e: any) {
+        error.value = e.message;
+      }
+    };
 
     const loadBalance = async () => {
       try {
-        await initTaquin();
+
         // @ts-ignore
-        const bal = await taquin.tolar.request({method: 'tol_getCurrentBalance'});
+     const bal = await taquin.tolar.getCurrentBalance();
 
         balance.value = (Number(bal) / 1e18).toFixed(2);
+
+      } catch (e: any) {
+        error.value = e.message;
+      }
+    };
+
+    const loadNetwork = async () => {
+      try {
+
+        // @ts-ignore
+     const net = await taquin.tolar.getNetwork();
+
+        network.value = net.id;
+
       } catch (e: any) {
         error.value = e.message;
       }
     };
 
     const send = async () => {
+
       error.value = '';
       success.value = '';
-      if (!account) {
-        error.value = 'Wallet account not found';
+
+      if (!walletConnected.value) {
+        error.value = 'Please connect wallet first';
         return;
       }
+
       if (!receiver.value || !amount.value) {
         error.value = 'Please enter receiver and amount';
         return;
       }
 
       try {
-        // @ts-ignore
-        await loadBalance();
 
         const tx = {
           receiverAddress: receiver.value,
           amount: (Number(amount.value) * 1e18).toString(),
-          networkId: NetworkId.Testnet
+          networkId: network.value || NetworkId.Testnet
         };
 
         // @ts-ignore
-        const hash = await taquin.tolar.request({
-          method: 'tol_sendRawTransaction',
-          params: [tx]
-        });
+       const hash = await taquin.tolar.sendRawTransaction(tx);
 
         success.value = `Sent! Transaction hash: ${hash}`;
 
-        // Reset input
         receiver.value = '';
         amount.value = '';
 
-        // Refresh balance
         await loadBalance();
+
       } catch (e: any) {
         error.value = e.message;
       }
     };
 
-    // Auto-load balance on mount
-    onMounted(loadBalance);
-
-    return { receiver, amount, balance, error, success, send };
+    return {
+      receiver,
+      amount,
+      balance,
+      error,
+      success,
+      send,
+      network,
+      connectWallet,
+      walletConnected,
+      account
+    };
   },
 });
 </script>
